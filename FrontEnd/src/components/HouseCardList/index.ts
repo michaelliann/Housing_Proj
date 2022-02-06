aff074479c1f65d35402bd2db00ddf3bb9b7438a
dhylan01 <dhylan01@users.noreply.github.com>
2022-02-05 18:05:16 -0800

    update with new master changes

--- FrontEnd/src/components/HouseCardList/index.ts
+++ FrontEnd/src/components/HouseCardList/index.ts
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 export { default } from './HouseCardList';
 export * from './HouseCardList';
 
+=======
+export { default } from './';
+export * from './';
+>>>>>>> b1c29796 (update with new master changes)
 export * from './HouseCard';
