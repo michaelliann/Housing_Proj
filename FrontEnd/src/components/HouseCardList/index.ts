328e7f8e498c07f7da2b560830a7afd9582f87a7
dhylan01 <dhylan01@users.noreply.github.com>
2022-02-05 21:49:15 -0800

    fix: changed set-up to work on Windows computers
    Edit the ReadMe file for set up with the correct commands
    Edit the dockerfile to have the right port

--- FrontEnd/src/components/HouseCardList/index.ts
+++ FrontEnd/src/components/HouseCardList/index.ts
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 export { default } from './HouseCardList';
 export * from './HouseCardList';
 
@@ -6,4 +7,9 @@ export * from './HouseCardList';
 export { default } from './';
 export * from './';
 >>>>>>> b1c29796 (update with new master changes)
+=======
+export { default } from './HouseCardList';
+export * from './HouseCardList';
+
+>>>>>>> 2d940a98 (fix: changed set-up to work on Windows computers)
 export * from './HouseCard';
