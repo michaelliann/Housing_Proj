d88c9a6967f29111bec073f1c58cf8549c73f8d4
dhylan01 <dhylan01@gmail.com>
2022-02-13 08:18:31 -0800

    fix: errors when runnning

--- FrontEnd/src/components/HouseCardList/index.ts
+++ FrontEnd/src/components/HouseCardList/index.ts
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 export { default } from './HouseCardList';
 export * from './HouseCardList';
 
-=======
-export { default } from './';
 export * from './';
->>>>>>> b1c29796 (update with new master changes)
-=======
-export { default } from './HouseCardList';
-export * from './HouseCardList';
 
->>>>>>> 2d940a98 (fix: changed set-up to work on Windows computers)
 export * from './HouseCard';
